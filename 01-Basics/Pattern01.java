a8d7123c4479e42cbd381c1ea559b8f34118f51b
Sanket Palankar <sanketpalankar12345@gmail.com>
2026-08-29 11:35:24 +0530

    "Solved Stivers Pattern 01: Rectangular Star Pattern (Updated code)"

--- 01-Basics/Pattern01.java
+++ 01-Basics/Pattern01.java
@@ -2,7 +2,7 @@ class Pattern01 {
     public static void main(String[] args) {
          int n = 5;
         for(int i =0 ; i <n ; i++){
-            System.out.println("*****"); 
+            System.out.println("*****");
         }
     }
 }

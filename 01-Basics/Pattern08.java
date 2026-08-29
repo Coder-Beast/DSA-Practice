2d2ff0f3f9a8762c542459dc072b1fbfc7cdbc36
Sanket Palankar <sanketpalankar12345@gmail.com>
2026-08-29 21:08:02 +0530

    Solved pattern 6,7,8

---
+++ 01-Basics/Pattern08.java
@@ -0,0 +1,19 @@
+class Pattern08{
+    public static void main(String[] args) {
+         int n =5;
+         int row = n;
+         for(int i = 0 ; i < n; i++){
+            
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }  
+            for(int j = 0 ; j <2 * row - 1 ; j++){ //striver used 2 * N - (2 * i + 1)
+                System.out.print("*");
+            }   
+            
+            row -= 1;
+            System.out.println(""); 
+         }
+        
+    }
+}
